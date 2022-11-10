8d684e7408a8ea472e42f61e953170a0faf7f4d0
Jeong Dong <eejxdong99@gmail.com>
2022-11-10 14:31:28 +0900

    feat: 메인 뷰에 식단 추가 버튼 구현

--- BoSS_CAD/Controllers/MainViewController.swift
+++ BoSS_CAD/Controllers/MainViewController.swift
@@ -26,6 +26,7 @@ class MainViewController: UIViewController {
         tableView.dataSource = self
         tableView.delegate = self
         tableView.rowHeight = 130
+        tableView.register(UINib(nibName: "PlusCell", bundle: nil), forCellReuseIdentifier: "PlusCell")
     }
     
     func setupNavigationBar() {
@@ -51,24 +52,34 @@ class MainViewController: UIViewController {
 
 extension MainViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return mealTimeArray.count
+        return mealTimeArray.count + 1
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "MenuCell", for: indexPath) as! MenuCell
         
-        cell.mealTimeLabel.text = mealTimeArray[indexPath.row]
-        
-        return cell
+        if (indexPath.row == mealTimeArray.count) {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "PlusCell", for: indexPath) as! PlusCell
+            return cell
+        } else {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "MenuCell", for: indexPath) as! MenuCell
+            
+            cell.mealTimeLabel.text = mealTimeArray[indexPath.row]
+            return cell
+        }
     }
 }
 
 extension MainViewController: UITableViewDelegate {
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        let storyboard = UIStoryboard(name: "Main", bundle: nil)
-        let detailVC = storyboard.instantiateViewController(withIdentifier: "DetailVC") as! DetailViewController
+        if(mealTimeArray.count == indexPath.row) {
+            print("plus")
+        } else {
+            let storyboard = UIStoryboard(name: "Main", bundle: nil)
+            let detailVC = storyboard.instantiateViewController(withIdentifier: "DetailVC") as! DetailViewController
+            
+            detailVC.mealTimeText = mealTimeArray[indexPath.row]
+            navigationController?.pushViewController(detailVC, animated: true)
+        }
         
-        detailVC.mealTimeText = mealTimeArray[indexPath.row]
-        navigationController?.pushViewController(detailVC, animated: true)
     }
 }
